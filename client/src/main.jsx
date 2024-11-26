6e2443f3dcdadf10e0a046a782a733ed15cb4382
YksRocks <73733249+YksRocks@users.noreply.github.com>
2024-11-27 04:07:26 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { CSSReset, theme, ChakraProvider, ColorModeProvider, ThemeProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+  CSSReset,
+  theme,
+  ChakraProvider,
+  ColorModeProvider,
+  ThemeProvider,
+} from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 const serverLocal = "https://urlshortener-n47w.onrender.com";
-const serverBaseUrl = import.meta.env.VITE_APP_URI;
+const serverBaseUrl = "https://urlshortener-n47w.onrender.com";
 axios.defaults.baseURL = serverBaseUrl;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <BrowserRouter>
-                <ThemeProvider theme={ theme }>
-                    <ColorModeProvider>
-                        <CSSReset />
-                        <App />
-                    </ColorModeProvider>
-                </ThemeProvider>
-            </BrowserRouter>
-        </ChakraProvider>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <ColorModeProvider>
+            <CSSReset />
+            <App />
+          </ColorModeProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
